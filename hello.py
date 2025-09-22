6befb3d20f7ad03ea7749886fa0fb61193f65391
virsworld <virpatel71@gmail.com>
2025-09-22 01:11:51 -0400

    activity 1.3 complete

--- hello.py
+++ hello.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', current_time=datetime.now())
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1> Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
